5f1113bce7e7b18ff6de37fc95e3917754e41936
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2024-07-18 15:42:18 +0300

    add links to the navigation

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
                 <h2>ALL new games are</h2>
                 <h3>Only in GamesPlay</h3>
             </div>
-            <img src="./images/four_slider_img01.png" alt="hero" />
+            <img src="/images/four_slider_img01.png" alt="hero" />
             <div id="home-page">
                 <h1>Latest Games</h1>
                 {/* Display div: with information about every game (if any) */}
