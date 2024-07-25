21d85bbbd41e86da049313d6a7534d1239ff6d21
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2024-07-25 14:11:09 +0300

    update custom hook useGetAllGames

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,4 +1,3 @@
-import useFetch from '../../hooks/useFetch';
 import { useGetAllGames } from '../../hooks/useGames';
 import Spinner from '../spinner/Spinner';
 import GameItem from './gameItem/GameItem';
@@ -11,12 +10,9 @@ export default function Catalog() {
             <h1>All Games</h1>
             {/* Display div: with information about every game (if any) */}
 
-            {isFetching
-                ? <Spinner />
-                : games.map(game => <GameItem key={game._id} {...game} refetch={refetch} />)
-                // {/* : <h3 className="no-articles">No articles yet</h3> */}
-            }
-
+            {games.length > 0
+                ? games.map(game => <GameItem key={game._id} {...game} />)
+                : <h3 className="no-articles">No articles yet</h3>}
 
         </section>
     );
--- client/src/hooks/useGames.js
+++ client/src/hooks/useGames.js
@@ -1,19 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import gamesAPI from "../api/games-api";
 
 export function useGetAllGames() {
     const [games, setGames] = useState({});
 
     useEffect(() => {
-
-        setIsFetching(true);
-
         (
             async () => {
                 const result = await gamesAPI.getAll();
 
-                setGames(result); 
+                setGames(result);
             }
         )()
     });
+
+    return [games, setGames];
 }
